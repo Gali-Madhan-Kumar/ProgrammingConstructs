2beffbf24a3337d77a7b07a9871c9f8779d682e9
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2020-09-02 21:31:19 +0530

    added code to flip coin multiple times

--- FlipCoinSimulation/flipCoinSimulator.sh
+++ FlipCoinSimulation/flipCoinSimulator.sh
@@ -1,10 +1,19 @@
 #! /bin/bash
 
 heads=0
-flipCoin=$((RANDOM % 2))
-if [ $flipCoin -eq heads ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+tails=0
+
+counter=0
+
+while [ counter < 5 ]
+do
+
+	flipCoin=$((RANDOM % 2))
+
+	if [ $flipCoin -eq heads ]
+	then
+		((heads = $((heads + 1))))
+	else
+		((tails = $((tails + 1))))
+	fi
+done
