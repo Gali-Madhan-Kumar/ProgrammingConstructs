bda59993cfa8af30364dd3b7f5c437f204184bb4
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2020-09-03 02:16:00 +0530

    added code to show heads or tails

--- FlipCoinCombination/flipCoinCombination.sh
+++ FlipCoinCombination/flipCoinCombination.sh
@@ -1,13 +1,41 @@
 #! /usr/local/bin/bash
 
+declare -A flipCoinDict
+
 isHeads=0
-isTails=1
 
-flipCoin=$((RANDOM % 2))
+tails=0
+heads=0
 
-if [ $flipCoin -eq $isHeads ]
-then
-	echo "Heads"
-else 
-	echo "Tails"
-fi
+flipCoinDict[heads]=0
+flipCoinDict[tails]=0
+
+i=0
+
+while [ $i -lt 2 ]
+do
+	flipCoin=$((RANDOM % 2))
+
+	if [ $flipCoin -eq $isHeads ]
+	then
+		((heads += $((heads + 1))))
+		flipCoinDict[heads]=$heads
+	else 
+		((tails += $((tails + 1))))
+		flipCoinDict[tails]=$tails
+	fi
+((i++))
+done
+
+totalHeads=${flipCoinDict[heads]}
+totalTails=${flipCoinDict[tails]}
+
+for ((j=0; j<$totalHeads; j+=1))
+do
+	echo -n "H " 
+done
+
+for ((k=0; k<$totalTails; k+=1))
+do
+	echo -n "T "
+done
